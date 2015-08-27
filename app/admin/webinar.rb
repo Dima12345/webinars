b913853ae707d1e0d60d56977c6bdd739157269c
Dima <gunosada1@gmail.com>
2015-08-27 11:37:55 +0300

    fix bug with permit

--- app/admin/webinar.rb
+++ app/admin/webinar.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Webinar do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :name, :subname, :data, :time, :city, :descriptions, :link_to_video
   #
   # or
   #
